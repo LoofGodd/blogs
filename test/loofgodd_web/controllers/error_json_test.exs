5bbbd392208698643c20ba274b1a9aa4748646bb
Chorn D. Bunnarot <chorndy.bunnarot@gmail.com>
2025-06-19 20:06:25 +0700

    first commit

---
+++ test/loofgodd_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule LoofgoddWeb.ErrorJSONTest do
+  use LoofgoddWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert LoofgoddWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert LoofgoddWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
