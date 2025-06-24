ff4d319d6f9401e0bbbbfda059e326fcb47d76ca
Chorn D. Bunnarot <chorndy.bunnarot@gmail.com>
2025-06-24 14:44:55 +0700

    feat: rating

--- lib/loofgodd/content/rating.ex
+++ lib/loofgodd/content/rating.ex
@@ -1,12 +1,13 @@
 defmodule Loofgodd.Content.Rating do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Loofgodd.Repo
 
   schema "ratings" do
     field :rating, :integer
     field :review, :string
-    field :post_id, :id
-    field :user_id, :id
+    belongs_to :post, Loofgodd.Blog.Post
+    belongs_to :user, Loofgodd.Accounts.User
 
     timestamps(type: :utc_datetime)
   end
@@ -14,7 +15,13 @@ defmodule Loofgodd.Content.Rating do
   @doc false
   def changeset(rating, attrs) do
     rating
-    |> cast(attrs, [:rating, :review])
-    |> validate_required([:rating, :review])
+    |> cast(attrs, [:rating, :review, :post_id, :user_id])
+    |> validate_required([:rating, :review, :post_id, :user_id])
+  end
+
+  def upsert(rating, attrs) do
+    rating
+    |> changeset(attrs)
+    |> Repo.insert_or_update()
   end
 end
